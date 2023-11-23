ad8771fa99da1e101e2e4e41d09c442e2ed62c95
Loïc PÉRON <loic.peron@gmail.com>
2023-11-23 16:34:06 +0100

    bootstrap: add 7zip scripts

--- libexec/scoop-bootstrap.ps1
+++ libexec/scoop-bootstrap.ps1
@@ -2,8 +2,25 @@
 
 . "$PSScriptRoot\..\lib\buckets.ps1"
 
-$BucketPath = Join-Path (Find-BucketDirectory "main") "bucket"
-New-Item -ItemType Directory -Path $BucketPath
-Set-Location $BucketPath
-curl.exe -O "https://raw.githubusercontent.com/ScoopInstaller/Main/master/bucket/git.json"
-curl.exe -O "https://raw.githubusercontent.com/ScoopInstaller/Main/master/bucket/7zip.json"
+$MainPath = (Find-BucketDirectory -Name "main" -Root)
+$BucketPath = Join-Path -Path $MainPath -ChildPath "bucket"
+New-Item -ItemType Directory -Path $BucketPath -Force | Out-Null
+$ScriptsPath = Join-Path -Path $MainPath -ChildPath "scripts"
+New-Item -ItemType Directory -Path $ScriptsPath -Force | Out-Null
+
+function Get-RemoteFile() {
+    param (
+        [Parameter(Mandatory)] $Url,
+        [Parameter(Mandatory)] $DestinationPath
+    )
+    if ($IsMacOS -Or $IsLinux) {
+        curl --output-dir $DestinationPath -O -C - -L -# $Url
+    } else {
+        curl.exe --output-dir $DestinationPath -O -C - -L -# $Url
+    }
+}
+
+Get-RemoteFile "https://raw.githubusercontent.com/ScoopInstaller/Main/master/bucket/git.json" $BucketPath
+Get-RemoteFile  "https://raw.githubusercontent.com/ScoopInstaller/Main/master/bucket/7zip.json" $BucketPath
+Get-RemoteFile  "https://raw.githubusercontent.com/ScoopInstaller/Main/master/scripts/install-context.reg" $ScriptsPath
+Get-RemoteFile  "https://raw.githubusercontent.com/ScoopInstaller/Main/master/scripyts/uninstall-context.reg" $ScriptsPath
