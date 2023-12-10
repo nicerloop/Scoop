17d91d6d3a96bba1ab7fb2588f7e253aa331a8d0
Loïc PÉRON <loic.peron@gmail.com>
2023-12-10 08:55:38 +0100

    bootstrap: equivalent to installer script

---
+++ libexec/scoop-bootstrap.ps1
@@ -0,0 +1,15 @@
+# Summary: Setup scoop shim and main bucket as does the Scoop installer
+
+. "$PSScriptRoot\..\lib\buckets.ps1"
+. "$PSScriptRoot\..\lib\core.ps1"
+. "$PSScriptRoot\..\lib\decompress.ps1"
+. "$PSScriptRoot\..\lib\install.ps1"
+
+Write-Host 'Creating scoop shim...'
+shim "$(currentdir 'scoop')\bin\scoop.ps1" $false
+
+Write-Host 'Seeding main bucket...'
+$MainBucketPath = ensure (Find-BucketDirectory -Name 'main' -Root)
+$MainBucketArchive = Join-Path $MainBucketPath "scoop-main.zip"
+Start-Download 'https://github.com/ScoopInstaller/Main/archive/refs/heads/master.zip' $MainBucketArchive
+Expand-ZipArchive -Path $MainBucketArchive -DestinationPath $MainBucketPath -ExtractDir "Main-master" -Removal
