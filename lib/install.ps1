ebaaa8648306fda3d6bca7f9811fa6e35a5b90d5
Loïc PÉRON <loic.peron@inetum.com>
2023-12-26 18:46:43 +0100

    wsl: convert path for windows host in env_add

--- lib/install.ps1
+++ lib/install.ps1
@@ -1005,6 +1005,7 @@ function env_set($manifest, $dir, $global, $arch) {
         $env_set | Get-Member -Member NoteProperty | ForEach-Object {
             $name = $_.name;
             $val = format $env_set.$($_.name) @{ "dir" = $dir }
+            if ($IsWSL) { $val = win_path $val }
             env $name $global $val
             Set-Content env:\$name $val
         }
